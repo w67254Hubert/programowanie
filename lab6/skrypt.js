451c27074825a69d8b56423383228fca7eb4098e
w67254Hubert <w67254@student.wsiz.edu.pl>
2023-04-25 20:17:38 +0200

    dokończone minimum lab 6

--- lab6/skrypt.js
+++ lab6/skrypt.js
@@ -110,13 +110,106 @@ console.log(tr1.nazwa);
 console.log(tr1.pole());
 
 
-//zadanie 4 dokończ
-// function PorFig(f1,f2,f3)
-// if(f1.pole()!= f2.pole()){
-//     if(f1.pole()>f2.pole()){
-//         console.log("większa figura " +f1.nazwa + "o polu"+ f2.pole());
-//     }
+// zadanie 4 
+function PorFig (f1, f2,f3){
+    if(f1.pole()==f2.pole() && f1.pole()==f3.pole() ){
+        console.log("są równe")
+    }else{
+        if(f1.pole()>f2.pole() && f1.pole()>f3.pole() ){
+            console.log("większa jest fugura " + f1.nazwa+ " o polu "+ f1.pole());
 
+        }else if (f2.pole()>f1.pole() && f2.pole()>f3.pole()){
+            console.log("większa jest fugura " + f2.nazwa+ " o polu "+ f2.pole());
+
+        }else if (f3.pole()>f1.pole() && f3.pole()>f2.pole()){
+            console.log("większa jest fugura " + f3.nazwa+ " o polu "+ f3.pole());
+        }
+        else if(f1.pole()==f2.pole()){
+                console.log(f1.nazwa+ " i " +f2.nazwa+" są równe i wynoszą "+f1.pole())
+        }else if(f1.pole()==f3.pole()){
+            console.log(f1.nazwa+ " i " +f3.nazwa+" są równe i wynoszą "+f1.pole())
+        }else if(f2.pole()==f3.pole()){
+            console.log(f2.nazwa+ " i " +f3.nazwa+" są równe i wynoszą "+f2.pole())
+        }else{
+            console.log("jak tu doszło to chyba nie masz liczby w tym XD")
+        }
+    }
 }
-// PorFig(p2,t2,tr2)
 
+PorFig(p2,t2,tr2)
+
+console.log("----------------------------pdf2");
+
+
+//lab6+ zadanie 1
+const tab1 = [1, 2, 3, 4]; 
+//sumowanie
+var sum = 0;
+var i = 0;
+while (i < tab1.length) {
+  sum += tab1[i];
+  i++;  
+}
+console.log(sum+" suma tablicy ");
+//parzyste pokarz
+var i = 0;
+while (i < tab1.length) {
+  if(tab1[i]%2)
+  console.log(tab1[i]+" jest parzysta ");
+  i++;
+}
+//mnorzenie *3
+var i = 0;
+while (i < tab1.length) {
+        tab1[i]=tab1[i]*3;
+  i++;
+}
+console.log(tab1)
+
+//dodanie nr alb i jego index
+tab1.push(67254)
+
+var i = 0;
+while (i < tab1.length) {
+    if(tab1[i]==67254){
+        console.log("numer almumu jesnt na indeksie nr: "+i )
+    }
+    i++;
+}
+//średnia arytmetyczna tablicy?
+
+var sum = 0;
+var i = 0;
+while (i < tab1.length) {
+  sum += tab1[i];
+  i++;  
+}
+aryt=sum/tab1.length;
+console.log("średnia arytmetyczna tablicy wynosi "+aryt)
+
+//max liczba z tab
+
+var i = 1;
+var max=tab1[0];
+while(i < tab1.length){
+    if (max<tab1[i]){
+
+        max=tab1[i]
+    }
+    i++;
+}
+console.log("max liczba w tablicy wynosi: "+ max)
+
+//ile razy wystapiła 2??
+
+const tab2 = [1, 2, 3,2 ,2, 4]; 
+
+var i = 0;
+var x=0
+while(i < tab2.length){
+    if (tab2[i]==2){
+        x++;
+    }
+    i++;
+}
+console.log("2 wystąpiła "+ x +" razy w tablicy")
