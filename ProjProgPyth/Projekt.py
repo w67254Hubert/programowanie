436dfd3b6d383db9a002a03964745c40b77cd7d3
w67254Hubert <w67254@student.wsiz.edu.pl>
2024-05-17 17:07:19 +0200

    proj start

---
+++ ProjProgPyth/Projekt.py
@@ -0,0 +1,35 @@
+# Dobrze przygotowany projekt powinien mieć:
+# - zaczyt/odczyt danych z pliku (JSON, CSV, inne) lub urządzenia 
+# - przetwarzanie danych 
+# - wykresy/analiza danych Opis projektu:
+# 0. Strona tytułowa 
+# 1. Opis produktu 
+# 2. Schematy akcji lub przypadków użycia 
+# 3. Prezentacja systemu 
+# 4. Bibliografia Styl zgodny ze stylem pracy inż. 
+# Ocena składa się z: 
+# - oceny działającego programu (prezentowanego po dowolnym laboratorium lub na ostatnich zajęciach projektowych) 
+# UWAGA. Można pracować w zespołach dwuosobowych. W takich przypadkach wszyscy autorzy zamieszczają komplet plików na moodlu.
+# Brak plików oznacza ocenę niedostateczną. Proszę prac nie wysyłać na maila.
+
+#https://www.kaggle.com/datasets/jacksondivakarr/car-crash-dataset/data
+#Wczytywanie, analiza i wizualizacja danych wypadków samochodowych. język Python
+
+#wczytaj csv i utwurz plik do zapisu
+#zapisz odpowiedzi na pytania w plikach
+
+#klasa z zapytaniami 
+
+#1 ile było procentowo przypadków niepeoprawnej jazdy
+
+#2 jaki procent wypadków miały wypadki zawierające więcej niż 3 samochody
+#wykres słupokowy ilości wypadków w zależności od pojazdów uszkodzonych w wypadku?
+
+#3 rodzaje wykroczeń które doprowadziły do wypadku i ich ilość
+
+#4 ile jest/jaki procent rekordów tworzą z znanym uszkodzeniem ciała injury/unknown
+
+#5 kiedy zdarza się więcej wypadków na dzień w dnie pracy czy weekendy
+#6 w jakich godzinach dochodzi do największej liczby wypadków albo wykres liniowy z godzinammi ilością wypadków podczas nich.
+
+#7 bedziesz miec godziny wypadkow np to predykcje mozesz zrobic np ze od godzin 0-4 najczesciej jest distracted kierowca
